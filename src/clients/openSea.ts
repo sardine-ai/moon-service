4ad8ea867b618b8858d166b74ed33c15277c0a1d
ethanitosardine <103773744+ethanitosardine@users.noreply.github.com>
2022-10-12 16:18:46 -0400

    adding buy NFT on opensea

--- src/clients/openSea.ts
+++ src/clients/openSea.ts
@@ -25,9 +25,12 @@ export class OpenSeaClient implements IOpenSeaClient {
     return order;
   }
 
-
-  async buyNFT() {
-    
-
+  async buyNFT(nftId: number, assetContractAddress: string, paymentTokenAddress: string) {
+    const order = await this.getOrder(nftId, assetContractAddress, paymentTokenAddress);
+    const tx = await this.openSea.fulfillOrder({
+      order,
+      accountAddress, // The address of your wallet, which will sign the transaction
+      recipientAddress // The address of the recipient, i.e. the wallet you're purchasing on behalf of
+    })
   }
 }
