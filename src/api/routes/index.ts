51b4e91783fd183b2ff743ddd1d4c267956a28b8
ethanitosardine <103773744+ethanitosardine@users.noreply.github.com>
2022-10-17 16:11:55 -0400

    reverted to wrokign state

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,11 +1,11 @@
-// import { buyNFT } from '../controllers';
+import { buyGenieNFT, buyClubHouseNFT } from '../controllers';
 import { BuyClubHouseNFTParams } from '../../types/clubHouse';
 import { validationMw } from '../middleware/validation';
 import express from 'express';
 
 const router = express.Router();
 
-// router.post('nft', validationMw(BuyClubHouseNFTParams), buyNFT);
-// router.post('/blockchain_transaction/{id}/status', validationMw(BuyClubHouseNFTParams), buyNFT);
+router.post('/buy-genie-nft', buyGenieNFT);
+router.post('/buy-club-house-nft', validationMw(BuyClubHouseNFTParams), buyClubHouseNFT);
 
 export default router;
