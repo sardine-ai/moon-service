96734a4a8dc67aa4429da8031ab2982c036eeeb9
ethanitosardine <103773744+ethanitosardine@users.noreply.github.com>
2022-11-15 16:53:59 -0500

    higher order controller function

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -2,35 +2,22 @@ import { Response, Request } from 'express';
 import * as commands from '../../commands/index';
 import { receiveFireblocksWebhook as receiveFireblocksWebhookCommand } from '../../commands/fireblocks-webhook'; 
 import Logger from '../../loaders/logger';
+import { CommandParams } from '../../types/command';
 
-export const buyGenieNFT = async (req: Request, res: Response) => {
+const commandTryCatchWrapper = (
+  command: (command: CommandParams) => Promise<void>
+) => async (req: Request, res: Response) => {
   try {
-    res.json(await commands.buyGenieNFT(req.body)).status(200);
+    res.json(await command(req.body)).status(200);
   } catch (err: unknown) {
     Logger.error("An Error Occured while processing Buy Genie NFT", err);
     res.status(500);
   }
 }
 
-export const buyClubHouseNFT = async (req: Request, res: Response) => {
-  try {
-    Logger.info("Handling Buy Club House NFT");
-    res.json(await commands.buyClubHouseNFT(req.body)).status(200);
-  } catch (err: unknown) {
-    Logger.error("An Error Occured while processing Buy Club House NFT", err);
-    res.status(500);
-  }
-}
-
-export const transferEvmFunds = async (req: Request, res: Response) => {
-  try {
-    Logger.info("Handling Transfer Funds");
-    res.json(await commands.transferFunds(req.body)).status(200);
-  } catch (err: unknown) {
-    Logger.error("An Error Occured while processing Handling Transfer Funds", err);
-    res.status(500);
-  }
-}
+export const buyGenieNFT = commandTryCatchWrapper(commands.buyGenieNFT);
+export const buyClubHouseNFT = commandTryCatchWrapper(commands.buyClubHouseNFT);
+export const transferEvmFunds = commandTryCatchWrapper(commands.transferFunds);
 
 export const receiveFireblocksWebhook = async (req: Request, res: Response) => {
   try {
