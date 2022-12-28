3bc0627998522a4c19e18c39db86eec4ae52940c
ethanitosardine <103773744+ethanitosardine@users.noreply.github.com>
2022-12-28 15:11:20 -0500

    Revert "Command Files and request types (#13)" (#17)
    This reverts commit 00853688c941c178446730b412c47652fcf69dd9.

--- src/types/command.ts
+++
@@ -1,8 +0,0 @@
-import { TransferEvmFundsParams } from './requests/transfer';
-import { BuyNftParams } from './requests/nft';
-import { Bundle } from './models';
-
-export type CommandParams = TransferEvmFundsParams &
-  BuyNftParams
-
-export type BuildBundle = (params: CommandParams) => Promise<Bundle>;
