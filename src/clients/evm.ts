b3fbed144111e6f054ca71a138673b8e1ae46514
ethanitosardine <103773744+ethanitosardine@users.noreply.github.com>
2023-01-03 11:28:53 -0500

    updated clients

--- src/clients/evm.ts
+++ src/clients/evm.ts
@@ -1,47 +1,86 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Erc20Token } from "../types/evm";
-import { getAssetContractDetails } from "../utils/crypto-utils";
-import { AbiItem } from "web3-utils"
-import abi from "../abi/ERC20ABI.json";
-import { Transaction, TransactionState, Operation } from "../types/models";
-import { v4 as uuidV4 } from "uuid";
-import { TransferEvmFundsParams } from "../types/requests/transfer";
-import Web3 from "web3";
+import { Erc20Token } from '../types/evm';
+import {
+  getAssetDetails,
+  amountToSmallestDenomination,
+  isNativeToken
+} from '../utils/crypto-utils';
+import { AbiItem } from 'web3-utils';
+import abi from '../abi/ERC20ABI.json';
+import { Transaction, TransactionState, Operation } from '../types/models';
+import { v4 as uuidV4 } from 'uuid';
+import { TransferEvmFundsParams } from '../types/requests/transfer';
+import Web3 from 'web3';
 
 export const loadContract = (assetContractAddress: string): any => {
   const web3 = new Web3();
-  const contract = new web3.eth.Contract(abi as unknown as AbiItem, assetContractAddress);
+  const contract = new web3.eth.Contract(
+    abi as unknown as AbiItem,
+    assetContractAddress
+  );
   return contract;
-}
+};
 
-export const buildTransferErc20CallData = (toAddress: string, amount: number, assetContractDetails: Erc20Token): string => {
+export const buildTransferErc20CallData = (
+  toAddress: string,
+  amount: number,
+  assetContractDetails: Erc20Token
+): string => {
   const contract = loadContract(assetContractDetails.assetContractAddress);
-  const callData = contract.methods.transfer(toAddress, (amount * 10 ** assetContractDetails.decimals).toString()).encodeABI();
+  const callData = contract.methods
+    .transfer(
+      toAddress,
+      amountToSmallestDenomination(
+        amount,
+        assetContractDetails.decimals
+      ).toString()
+    )
+    .encodeABI();
   return callData;
-}
+};
 
 export const buildEvmTransferTransaction = ({
   toAddress,
   amountInAsset,
   chain,
-  assetSymbol,
+  assetSymbol
 }: TransferEvmFundsParams): Transaction => {
   const transaction: Transaction = {
     id: uuidV4(),
-    isStarting: false,
+    order: -1,
     state: TransactionState.CREATED,
     to: toAddress,
     chain: chain,
-    assetSymbol: assetSymbol,
     operation: Operation.TRANSFER_FUNDS
-  }
-  if (assetSymbol == "NATIVE") {
-    transaction.value = (amountInAsset * 10 ** 18).toString();
+  };
+  const assetContractDetails = getAssetDetails(chain, assetSymbol);
+  if (isNativeToken(assetSymbol)) {
+    const value = amountToSmallestDenomination(
+      amountInAsset,
+      assetContractDetails.decimals
+    )
+      .toString()
+      .toString();
+    transaction.value = value;
   } else {
-    const assetContractDetails = getAssetContractDetails(chain, assetSymbol);
-    transaction.callData = buildTransferErc20CallData(toAddress, amountInAsset, assetContractDetails);
+    transaction.callData = buildTransferErc20CallData(
+      toAddress,
+      amountInAsset,
+      assetContractDetails
+    );
     transaction.to = assetContractDetails.assetContractAddress;
   }
+  transaction.assetCosts = [
+    {
+      assetSymbol: assetSymbol,
+      amount: amountToSmallestDenomination(
+        amountInAsset,
+        assetContractDetails.decimals
+      ).toString(),
+      decimals: getAssetDetails(chain, assetSymbol).decimals
+    }
+  ];
+
   return transaction;
-}
+};
---
+++ src/clients/opensea.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+import { v4 as uuidV4 } from 'uuid';
+import { OpenSeaSDK } from 'opensea-js';
+import { BuyNftParams } from '../types/requests/nft';
+import { CryptoConfig } from '../config/crypto-config';
+import { Operation, Transaction, TransactionState } from '../types/models';
+import { NftNotFoundError } from '../types/errors';
+import { PopulatedTransaction } from 'ethers';
+
+export interface IOpenSeaClient {
+  ethOpenSea: OpenSeaSDK;
+  buildTransaction(params: BuyNftParams): Promise<Transaction>;
+}
+
+export class OpenSeaClient implements IOpenSeaClient {
+  cryptoConfig: CryptoConfig;
+  ethOpenSea: OpenSeaSDK;
+
+  constructor(cryptoConfig: CryptoConfig, ethOpenSea: OpenSeaSDK) {
+    this.cryptoConfig = cryptoConfig;
+    this.ethOpenSea = ethOpenSea;
+  }
+
+  async getOrder(nftId: string, assetContractAddress: string) {
+    try {
+      const order = await this.ethOpenSea.api.getOrder({
+        side: 'ask',
+        tokenId: nftId.toString(),
+        assetContractAddress: assetContractAddress
+      });
+      return order;
+    } catch (error) {
+      throw NftNotFoundError(error);
+    }
+  }
+
+  async getFulfillOrderCallData(
+    protocalData: any,
+    accountAddress: string,
+    recipientAddress: string
+  ): Promise<PopulatedTransaction> {
+    try {
+      const { actions } = await this.ethOpenSea.seaport.fulfillOrder({
+        order: protocalData,
+        accountAddress,
+        recipientAddress
+      });
+      const callData = await actions[0].transactionMethods.buildTransaction();
+      return callData;
+    } catch (error) {
+      throw NftNotFoundError(error);
+    }
+  }
+
+  async buildTransaction(buyNftParams: BuyNftParams): Promise<Transaction> {
+    const order = await this.getOrder(
+      buyNftParams.nftId,
+      buyNftParams.contractAddress
+    );
+    const callData = await this.getFulfillOrderCallData(
+      order.protocolData,
+      this.cryptoConfig.sardinePublicKey,
+      buyNftParams.recipientAddress
+    );
+    return {
+      id: uuidV4(),
+      order: -1,
+      state: TransactionState.CREATED,
+      to: callData.to!,
+      value: callData.value?.toString(),
+      callData: callData.data,
+      chain: buyNftParams.chain,
+      operation: Operation.BUY_NFT
+    };
+  }
+}
+
+export class TestOpenSeaClient implements IOpenSeaClient {
+  ethOpenSea: OpenSeaSDK;
+
+  async buildTransaction(buyNftParams: BuyNftParams): Promise<Transaction> {
+    return {
+      id: '0',
+      order: -1,
+      state: TransactionState.CREATED,
+      to: buyNftParams.contractAddress,
+      value: '1',
+      callData: '0xcallData',
+      chain: buyNftParams.chain,
+      operation: Operation.BUY_NFT
+    };
+  }
+}
